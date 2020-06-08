d848b20c702aa0297fa60dae9a8a4253f5a18b87
Andrea Harris <andrea@andreas-imac.lan>
2020-06-08 09:59:55 -0500

    2&3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,16 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    integers = {}
+    result = []
+    for x in arrays:
+        for y in x:
+            if y not in integers:
+                integers[y] = 0
+            else:
+                result.append(y) 
+    result= list(dict.fromkeys(result))
+
 
     return result
 
